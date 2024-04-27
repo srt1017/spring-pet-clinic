1b1d82575100cc05b5010ab594a96d08275d8abb
srt1017 <omarfarouq107@gmail.com>
2024-04-26 19:32:27 -0500

    Added POJOS. Closes #2

---
+++ spring-pet-clinic/src/main/java/com/example/springpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.example.springpetclinic.model;
+
+public class Owner extends Person{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ spring-pet-clinic/src/main/java/com/example/springpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package com.example.springpetclinic.model;
+
+public class Vet extends Person{
+    public static void main(String[] args) {
+
+    }
+}
